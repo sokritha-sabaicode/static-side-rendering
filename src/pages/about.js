3c2ddb84187826f0cc21e58492179189fbfeea7a
sokritha-it <sokritha.it@gmail.com>
2023-09-03 08:32:01 +0700

    fix error api call

--- src/pages/about.js
+++ src/pages/about.js
@@ -11,7 +11,7 @@ export default function About({ posts }) {
 // This function gets called at build time
 export async function getStaticProps() {
   // Call an external API endpoint to get posts
-  const res = await fetch("http://localhost:3000/api/posts");
+  const res = await fetch("/api/posts");
   const posts = await res.json();
   console.log(posts);
 
